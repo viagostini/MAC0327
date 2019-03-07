e69cd740aadf990df25f74d19e2f440463132d1b
mac0327 07 <mac0327.07@bacamarte.cec.ime.usp.br>
2019-03-07 11:37:33 -0300

    duvida no problema 3D

--- Aula03/3D.cpp
+++ Aula03/3D.cpp
@@ -2,15 +2,20 @@
 
 using namespace std;
 
-long long ans = 0;
-
 int main () {
-    long long n;
+    uint64_t n;
 
     cin >> n;
+    
+    uint64_t ans = 2;
+    ans <<= n;
+    ans -= 2ull;
 
+    uint64_t ans2 = 0;
     for (int i = 1; i <= n; i++)
-        ans += pow(2, i);
+        ans2 += pow(2,i);
 
     cout << ans << endl;
+    cout << ans2 << endl;
 }
+
