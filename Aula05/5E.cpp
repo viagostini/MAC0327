83b302e3c936394c9a10832bcef82187413472aa
viagostini <vinicius.agostini19@gmail.com>
2019-03-08 21:51:30 -0300

    5E accepted

---
+++ Aula05/5E.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main () {
+    int n, ans = 0;
+
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+        ans += (n-i)*i + 1;
+
+    cout << ans << endl;
+}
