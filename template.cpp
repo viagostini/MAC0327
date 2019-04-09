8938619cd7d6c35db58a59f447dfce4faf620023
viagostini <vinicius.agostini19@gmail.com>
2019-04-08 22:55:42 -0300

    update template

--- template.cpp
+++ template.cpp
@@ -5,10 +5,11 @@ using namespace std;
 typedef long long ll;
 
 #define mp make_pair
-#define f first
-#define s second
 #define pb push_back
-#define pair<int,int> pii;
+#define pair<int,int> pii
+#define all(c) c.begin(), c.end()
+#define FOR(i, k, n) for (int i = k; i < n; i++)
+#define ROF(i, k, n) for (int i = n-1; i >= k; i--)
 
 int main () {
     
